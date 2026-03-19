24f37178b84498ed7b2ee7a91e377fbb0a329423
Julian Ghadially <julianghadially@Julians-Mac-mini.local>
2026-03-19 01:43:06 -0500

    03-19

--- src/program/__init__.py
+++ src/program/__init__.py
@@ -1,4 +1,5 @@
 from src.program.counting_rm import CountingRM
 from src.program.phantomwiki_pipeline import PhantomWikiReActPipeline
 from src.program.phantomwiki_module import PhantomWikiReAct
-from src.program.baseline_rlm.rlm_module import PhantomWikiRLM, RLMPipeline
+from src.program.baseline_rlm.rlm_module import PhantomWikiRLM
+from src.program.baseline_rlm.rlm_pipeline import RLMPipeline
